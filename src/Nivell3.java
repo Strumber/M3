8373609a4f8c9788eb3b70c0f870a3ccc4272459
Strumber <mzgjar@protonmail.com>
2021-03-11 11:10:26 +0100

    Nivell3

---
+++ src/Nivell3.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.util.ArrayList;
+public class Nivell3 {
+
+	public static void main(String[] args) {
+		System.out.println("Seqüència de Fibonacci \n ");
+		int num = Integer.parseInt(JOptionPane.showInputDialog("Introdueix un nombre"));
+		
+		ArrayList<Integer>nums = new ArrayList<Integer>();
+		int f;
+		nums.add(0);
+		nums.add(1);
+		for (int i = 2 ; i<num ; i++) {
+			f= nums.get(i-2)+nums.get(i-1);
+			nums.add(i,f);
+		//System.out.print(f);
+		}
+		for (int Print:nums) {
+			System.out.print(Print+" ");
+		}
+
+	}
+
+}
